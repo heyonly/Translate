c631a109bb8db3aced1a05ea01d016488f71b932
haibing <yinhaibing@didiglobal.com>
2019-12-09 22:21:01 +0800

    删除无用代码

--- Translate/GUI/RootWXViewApplication.py
+++ Translate/GUI/RootWXViewApplication.py
@@ -47,45 +47,6 @@ class WxFrame(wx.Frame):
         right_dt = FileDrop(self.right_text)
         self.right_text.SetDropTarget(right_dt)
 
-def on_click(self):
-        pass
-
-
-class ProxyFrame(wx.ScrolledWindow):
-    def __init__(self, parent):
-        wx.ScrolledWindow.__init__(self, parent)
-        self.create_widget()
-
-    def create_widget(self):
-        self.proxy_split_mult = wx.SplitterWindow(self, style=wx.SP_LIVE_UPDATE,size=(800,600))
-        self.proxy_split_mult.SetMinimumPaneSize(100)
-
-        self.proxy_scroll_left = wx.ScrolledWindow(self.proxy_split_mult)
-        self.proxy_scroll_left.SetBackgroundColour(wx.WHITE)
-        self.proxy_scroll_left.SetScrollbars(0,0,400,600)
-        self.proxy_scroll_left.SetAutoLayout(1)
-
-        self.proxy_scroll_right = wx.ScrolledWindow(self.proxy_split_mult)
-        self.proxy_scroll_right.SetBackgroundColour(wx.BLACK)
-
-        self.proxy_split_mult.SplitVertically(self.proxy_scroll_left,self.proxy_scroll_right)
-        self.proxy_split_mult.SetSashGravity(0.5)
-
-class MainFrame(wx.Frame):
-    def __init__(self):
-        wx.Frame.__init__(self,None, -1, 'WxFrame',size=(800,600))
-        self.createWidget()
-
-    def createWidget(self):
-        self.statusbar = self.CreateStatusBar(2, wx.STB_SIZEGRIP)
-        self.statusbar.SetStatusText('0',0)
-        self.statusbar.SetStatusText('1',1)
-        self.createProxyWidget()
-
-    def createProxyWidget(self):
-        self.proxy_nb = wx.Notebook(self, -1, name="proxy_nb")
-        self.proxyFrame = ProxyFrame(self.proxy_nb)
-        self.proxy_nb.AddPage(self.proxyFrame, u"HTTP代理")
 
 class FileDrop(wx.FileDropTarget):
     def __init__(self, window):
@@ -98,14 +59,3 @@ class FileDrop(wx.FileDropTarget):
             print(name)
 
         return True
-
-
-class DropFile(wx.Frame):
-    def __init__(self, parent, id, title=None):
-        wx.Frame.__init__(self, parent, id, title, size=(450, 400))
-
-        self.text = wx.TextCtrl(self, -1, style=wx.TE_MULTILINE)
-        dt = FileDrop(self.text)
-        self.text.SetDropTarget(dt)
-        self.Centre()
-        self.Show(True)
